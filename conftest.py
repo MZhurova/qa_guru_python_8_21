18a0ac278e93102ecf836d344a318a22e45ae5d8
Mariya Zhurova <mzhurova4@mail.ru>
2024-01-08 05:34:27 +0700

    add

--- conftest.py
+++ conftest.py
@@ -19,8 +19,8 @@ def android_mobile_management():
         "deviceName": "Samsung Galaxy S23 Ultra",
 
         # Set URL of the application under test
-        "app": "bs://sample.app",
-
+        # "app": "bs://sample.app",
+        "app": "bs://04b49845ae18715f926fe263abfef9701aa7a4d6",
 
         # Set other BrowserStack capabilities
         'bstack:options': {
@@ -33,6 +33,60 @@ def android_mobile_management():
             "accessKey": config.access_key
         }
     })
+
+    # browser.config.driver_remote_url = remote_browser_url
+    # browser.config.driver_options = options
+
+    with allure.step('setup app session'):
+        browser.config.driver = webdriver.Remote(
+            config.remote_browser_url,
+            options=options
+        )
+
+    browser.config.timeout = 10.0
+
+    browser.config._wait_decorator = support._logging.wait_with(
+        context=allure_commons._allure.StepContext)
+
+    yield
+
+    allure_attach.screenshot()
+
+    allure_attach.page_source_xml()
+
+    session_id = browser.driver.session_id
+
+    with allure.step('tear down app session'):
+        browser.quit()
+
+    allure_attach.bstack_video(session_id)
+
+
+@pytest.fixture(scope='function')
+def ios_mobile_management():
+    options = XCUITestOptions().load_capabilities({
+        # Specify device and os_version for testing
+        "platformName": "ios",
+        "platformVersion": "16",
+        "deviceName": "iPhone 14 Pro Max",
+
+        # Set URL of the application under test
+        "app": "bs://444bd0308813ae0dc236f8cd461c02d3afa7901d",
+
+        # Set other BrowserStack capabilities
+        'bstack:options': {
+            "projectName": "Ios tests",
+            "buildName": "browserstack-simple-app-build",
+            "sessionName": "BStack Simple app test",
+
+            # Set your access credentials
+            "userName": config.username,
+            "accessKey": config.access_key
+        }
+    })
+
+    # browser.config.driver_remote_url = remote_browser_url
+    # browser.config.driver_options = options
 
     with allure.step('setup app session'):
         browser.config.driver = webdriver.Remote(
