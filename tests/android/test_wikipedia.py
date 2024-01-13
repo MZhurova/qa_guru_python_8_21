c16b9db5c2b3cb327a0f08087b89fa3e87b923b8
Mariya Zhurova <mzhurova4@mail.ru>
2024-01-13 20:42:33 +0700

    add

--- tests/android/test_wikipedia.py
+++ tests/android/test_wikipedia.py
@@ -4,8 +4,6 @@ from selene import browser, have
 
 
 def test_search(android_mobile_management):
-    # with step('Tap on the skip button'):
-    #     browser.element((AppiumBy.ID, 'org.wikipedia:id/fragment_onboarding_skip_button')).click()
     with step('Type search'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Appium')
@@ -17,8 +15,6 @@ def test_search(android_mobile_management):
 
 
 def test_search_google(android_mobile_management):
-    # with step('Tap on the skip button'):
-    #     browser.element((AppiumBy.ID, 'org.wikipedia:id/fragment_onboarding_skip_button')).click()
     with step('Type search "Google"'):
         browser.element((AppiumBy.ACCESSIBILITY_ID, "Search Wikipedia")).click()
         browser.element((AppiumBy.ID, "org.wikipedia.alpha:id/search_src_text")).type('Google')
